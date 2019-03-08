53652a2008966e52ca4240da9730d38f0913a808
Jackson Prince <jdprince555@gmail.com>
2019-03-08 22:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object;
+  Object.assign({}, object, {key: value});
+    return object;
 }
 
 updateObjectWithKeyAndValue(recipes, "fun", "surprise");
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, object, {key: value});
-}
